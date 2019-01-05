26466413afce677f3a9d6f2f30ddb65a3a86ae38
pzoechner <p.zoechner@gmail.com>
2019-01-05 16:31:25 +0100

    Showing simple custom post type data

---
+++ loop-templates/content-event.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * Single post partial template.
+ *
+ * @package understrap
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+
+<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+
+	<header class="entry-header">
+
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+
+		<div class="entry-meta">
+
+			<?php understrap_posted_on(); ?>
+
+		</div><!-- .entry-meta -->
+
+	</header><!-- .entry-header -->
+
+	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+
+	<div class="entry-content">
+
+		<?php the_content(); ?>
+
+
+		<?php
+			// 		echo var_dump(get_field(''));
+			// 		echo var_dump(get_field('country')[0]);
+			// 		echo "<br><br>";
+			echo 'Country: <a href="' . get_field('country')[0]->guid . '">' . get_field('country')[0]->post_title . '</a><br>';
+			echo 'Location: ' . get_field('location') . '<br>';
+			$countryId = get_field('country')[0]->ID;
+		?>
+
+
+		<?php
+		wp_link_pages(
+			array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			)
+		);
+		?>
+
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+
+		<?php understrap_entry_footer(); ?>
+
+	</footer><!-- .entry-footer -->
+
+</article><!-- #post-## -->
