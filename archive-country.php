4415e8cf44ec019e67b130cba0455d2abfbe0fb5
Phil Zoechner <p.zoechner@gmail.com>
2019-01-06 13:40:17 +0100

    Archive: Country styling

--- archive-country.php
+++ archive-country.php
@@ -31,7 +31,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 
 					<header class="page-header">
 						<?php
-						the_archive_title( '<h1 class="page-title">', '</h1>' );
+						the_archive_title( '<h1 class="page-title mb-4">', '</h1>' );
 						the_archive_description( '<div class="taxonomy-description">', '</div>' );
 						?>
 					</header><!-- .page-header -->
