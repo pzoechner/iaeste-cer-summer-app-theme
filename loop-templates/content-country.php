dce35ade7e9ecdb41173f2c3ed51b058f1b257ed
Phil Zoechner <p.zoechner@gmail.com>
2019-03-10 19:36:02 +0100

    Update content-country.php

--- loop-templates/content-country.php
+++ loop-templates/content-country.php
@@ -29,35 +29,66 @@ if ( ! defined( 'ABSPATH' ) ) {
 		<!-- PLAY GROUND -->
 		
 		<?php //echo var_dump(get_field('country')); ?>
-		<img style="width: 100px;" src="<?= get_field('country_flag') ?>">
 
-		<?php
-			$events = get_posts(array(
-				'post_type' => 'event',
-				'meta_query' => array(
-					array(
-						'key' => 'country',
-						'value' => get_the_ID(),
-						'compare' => '='
-					)
-				)
-			));
-		?>
-		<?php if( $events ): ?>
-			<br><br>
-			Events in this country:
-			<ul>
-			<?php foreach( $events as $event ): ?>
-				<li>
-					<a href="<?php echo get_permalink( $event->ID ); ?>">
-						<?php echo get_the_title( $event->ID ); ?>
-					</a>
-				</li>
-			<?php endforeach; ?>
-			</ul>
-		<?php endif; ?>
-		
-		<br><br><br><!-- /PLAY GROUND -->
+		<div class="container">
+  			<div class="row">
+			  	<div class="col jumbotron py-0">
+
+					<div class="my-5">
+						<h2>Country Information</h2>
+					</div>
+
+					<div class="my-4">
+						<h3 class="text-muted">Country Flag</h3>
+						<img class="rounded responsive" src="<?= get_field('country_flag') ?>">
+					</div>
+
+					<div class="my-4">
+						<h3 class="text-muted">Capital City</h3>
+						<p class="display-4"><?= get_field('capital_city') ?></p>
+					</div>
+
+					<div class="my-4">
+						<h3 class="text-muted">Population</h3>
+						<p class="display-4"><?= get_field('population') ?></p>
+					</div>
+
+				</div>
+				<div class="col">
+
+					<?php
+						// TODO add sorting, maybe also filtering by splitting upcoming and past events.
+						$events = get_posts(array(
+							'post_type' => 'event',
+							'meta_query' => array(
+								array(
+									'key' => 'country',
+									'value' => get_the_ID(),
+									'compare' => '='
+								)
+							)
+						));
+					?>
+
+					<?php if( $events ): ?>
+						<div class="mt-5 mb-2">
+							<h2>Events</h2>
+
+							<ul class="list-group">
+							<?php foreach( $events as $event ): ?>
+								<a href="<?php echo get_permalink( $event->ID ); ?>">
+									<li class="list-group-item"><?php echo get_the_title( $event->ID ); ?></li>
+								</a>
+							<?php endforeach; ?>
+							</ul>
+						</div>
+					<?php endif; ?>
+
+				</div>
+			</div>
+		</div>
+
+		<!-- /PLAY GROUND -->
 
 
 		<?php
